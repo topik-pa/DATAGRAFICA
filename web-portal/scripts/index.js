af9b71c0f3a1ebd4024cc76ca9eb7db8673c5bb9
Marco Pavan <marcopavan.mp@gmail.com>
2023-11-19 08:37:44 +0100

    no pooling

--- web-portal/scripts/index.js
+++ web-portal/scripts/index.js
@@ -1,10 +1,7 @@
-import { pool } from './pool.js'
 import mainMenu from '../components/shared/header/main-menu/main-menu.js'
 import cookieLayer from '../components/shared/cookie_layer/cookie_layer.js'
 import gotoTop from '../components/shared/goto_top/goto_top.js'
 import header from '../components/shared/header/header.js'
-
-pool()
 
 mainMenu.toggleMobileMenu()
 cookieLayer.init()
