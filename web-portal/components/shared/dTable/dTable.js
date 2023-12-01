86bc06c5cb478be51392a91c9665703bef7db5b3
Marco Pavan <marcopavan.mp@gmail.com>
2023-12-01 08:55:29 +0100

    Some fixes

--- web-portal/components/shared/dTable/dTable.js
+++ web-portal/components/shared/dTable/dTable.js
@@ -97,22 +97,23 @@ for (const $filter of $filters) {
 const getBoostData = (data) => {
   let str = ''
   data.forEach(boost => {
-    const startDate = new Date(boost.started)
-    const endDate = new Date((boost.started + (1000 * 60 * 60 * 24 * boost.days)))
+    const startDate = boost.started ? new Date(boost.started).toLocaleDateString() : null
+    const endDate = boost.started ? new Date((boost.started + (1000 * 60 * 60 * 24 * boost.days))).toLocaleDateString() : null
     const today = Date.now()
-    const engagement = ((boost.reactions / boost.followers) * 100).toFixed(2)
+    const engagement = (boost.reactions && boost.followers) ? ((boost.reactions / boost.followers) * 100).toFixed(2) : null
     str += `<div><div class="${today < endDate ? 'green' : 'red'}">&#9673;</div>`
     str += `${boost.price}€ (${boost.social})`
     str += '<br>'
     str += `Target: ${boost.target}`
     str += '<br>'
-    str += `Start: ${startDate.toLocaleDateString()}`
+    str += `Start: ${startDate}`
     str += '<br>'
-    str += `End: ${endDate.toLocaleDateString()}`
+    str += `End: ${endDate}`
     str += '<br>'
     str += `Cpi: ${boost.cpi || 'ND'}`
     str += '<br>'
-    str += `Engagement: ${engagement}%`
+    str += `Engagement: ${engagement || 'ND'} %`
+    str += '<hr>'
     str += '</div>'
   })
   return str
